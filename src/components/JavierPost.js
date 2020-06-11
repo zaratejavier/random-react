8b3491962b97b2b54b05c7882b5819210f7c6137
Javier Zarate <zarate.javier.jz@gmail.com>
2020-06-11 17:14:50 -0600

    Javier component ready to merge

--- src/components/JavierPost.js
+++ src/components/JavierPost.js
@@ -1,13 +1,60 @@
 import React from "react"
-import {Header, Segment,} from "semantic-ui-react"
+import {Link} from "react-router-dom"
+import {Header, Segment, Icon, Button} from "semantic-ui-react"
+import JavierForm from "./JavierForm";
 
 class JavierPost extends React.Component{
 
+  state = {editing: false,};
+
+  toggleEdit = () => this.setState({editing: !this.state.editing,});
+
+
+
 render(){
   return(
-    <Segment>
-      <Header as="h3">{this.props.name}</Header>
-  <p>{this.props.ingridients.substring(1,30)}...</p>
+    <Segment 
+      style={{
+        display: "flex", 
+        alignItems: "center", 
+        justifyContent: "space-between"
+      }}>
+      {this.state.editing ? (
+        <JavierForm
+          {...this.props}
+          edit={this.props.edit}
+          toggle={this.toggleEdit}
+          />
+      ) : (
+        <>
+        <Header as="h3">{this.props.name}</Header>
+        <p>{this.props.ingridients.substring(0,30)}...</p>
+        </>
+      )}
+      <div>
+        <Button icon color="blue" onClick={this.toggleEdit}>
+          <Icon name="pencil"/>
+        </Button>
+
+        <Button icon color="red">
+          <Icon name="trash"/>
+        </Button>
+        <Link to={{
+          pathname: `/Javier/${this.props.id}`,
+          state: {
+            key1:'whatever',
+            name: this.props.name,
+            ingridients: this.props.ingridients,
+            time : this.props.time,
+          }
+        }}>
+
+        <Button icon color="green">
+          <Icon name="eye"/>
+        </Button>
+        </Link>
+      </div>
+    
     </Segment>
   )
 }
